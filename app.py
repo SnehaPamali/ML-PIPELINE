3e1833a13e85e8608a6db8c61fc122818158a25f
SnehaPamali <snehakpamali@gmail.com>
2024-04-26 16:30:23 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from src.pipelines.pred_pipeline import CustomData, PredictPipeline
+from src.pipelines.prediction_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
